723a2bf37a774a31a260e569eb55086e1b4952d2
Daniel <i.daniil2010@yandex.ru>
2022-06-07 16:06:11 +0400

    licence

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 /*
 Copyright (C) THEDATE Ignatev Daniil
-This file is part of DIBotInstaller <https://github.com/>.
+This file is part of RudironInstaller <https://github.com/>.
 
-DIBotInstaller is free software: you can redistribute it and/or modify
+RudironInstaller is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-DIBotInstaller is distributed in the hope that it will be useful,
+RudironInstaller is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with DIBotInstaller. If not, see <http://www.gnu.org/licenses/>.
+along with RudironInstaller. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <QGuiApplication>
